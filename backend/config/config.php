fe726f238bf3de92e898d30078a26da671e61a3a
koddas <johan@idioti.se>
2014-03-26 23:47:19 +0100

    Lägg till config-filen igen

---
+++ backend/config/config.php
@@ -0,0 +1,12 @@
+<?php
+$config = array(
+	'slim' => array(),
+	'medoo' => array(
+		'database_type' => 'mysql',
+		'database_name' => '',
+		'server' => 'localhost',
+		'username' => '',
+		'password' => '',
+		'charset' => 'utf8'
+	)
+);
