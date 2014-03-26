d755b240fe4c934f41bfa2469495f63aaf6dcd74
koddas <johan@idioti.se>
2014-03-26 23:39:55 +0100

    oops

--- backend/config/config.php
+++ backend/config/config.php
@@ -3,10 +3,10 @@ $config = array(
 	'slim' => array(),
 	'medoo' => array(
 		'database_type' => 'mysql',
-		'database_name' => '176690-lasbok',
+		'database_name' => '',
 		'server' => 'localhost',
-		'username' => '176690_ss17621',
-		'password' => '994deenajs',
+		'username' => '',
+		'password' => '',
 		'charset' => 'utf8'
 	)
 );
