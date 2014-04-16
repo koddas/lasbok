f357626836ed6f099ad79c424bf9c5fe9cff8ca0
koddas <johan@idioti.se>
2014-04-16 10:17:39 +0200

    Fixade till lite buggar, fixade lite indentering.

--- backend/application/endpoints/category.php
+++ backend/application/endpoints/category.php
@@ -1,14 +1,14 @@
 <?php
 $app->get('/category', function () use ($app, $db) {
 	$cols = array('id', 'description');
-	$category = $db->select('Categry', $cols);
+	$category = $db->select('Customer_categories', $cols);
 	echo json_encode($buildings, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
 });
 
 $app->get('/category/:id', function ($id) use ($app, $db) {
 	$cols = array('id', 'description');
 	$where = array('id' => $id);
-	$category = $db->select('Categry', $cols, $where);
+	$category = $db->select('Customer_categories', $cols, $where);
 	
 	if (count($category) > 0) {
 		echo json_encode($category[0], JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
@@ -19,27 +19,24 @@ $app->get('/category/:id', function ($id) use ($app, $db) {
 
 $app->post('/category', function () use ($app, $db) {
 	// TODO: Kontrollera om användaren har behörighet att skapa categori
-	$id = $app->request->post('id');
 	$description = $app->request->post('description');
+		
+	$values = array('description' => $description);
 	
-	if (!(strlen($id) > 0 && strlen($description) > 0)) {
-		$app->halt(400, 'Bad request');
-		}
-		
-		$db->query("INSERT INTO category VALUES ('$id', '$description');");
-		
-		$errors = $db->error();
-		
-		switch (intval($errors[0])) {
-			case 0:
-				$app->response()->status(201);
-				break;
-			case 23000:
-				$app->halt(409, "Category '$id' already exists");
-				break;
-			default:
-				$app->halt(500, $errors[2]);
-		}
+	$db->insert('User_roles', $values);
+	
+	$errors = $db->error();
+	
+	switch (intval($errors[0])) {
+		case 0:
+			$app->response()->status(201);
+			break;
+		case 23000:
+			$app->halt(409, "Category '$description' already exists");
+			break;
+		default:
+			$app->halt(500, $errors[2]);
+	}
 		
 });
 
@@ -56,10 +53,10 @@ $app->put('/category/:id', function ($id) use ($app, $db) {
 		$app->halt(400, 'Bad request');
 	}
 	
-	$values = array('id' => $id,
-			'description' => $description);
+	$values = array('description' => $description);
+	$where = array('id' => $id);
 	
-	$db->insert('category', $values);
+	$db->update('Customer_categories', $values, $where);
 	
 	$errors = $db->error();
 	
@@ -68,7 +65,7 @@ $app->put('/category/:id', function ($id) use ($app, $db) {
 			$app->response()->status(201);
 			break;
 		case 23000:
-			$app->halt(409, "Category 'id' already exists");
+			$app->halt(409, "Category '$id' already exists");
 			break;
 		default:
 			$app->halt(500, $errors[2]);
@@ -78,6 +75,6 @@ $app->put('/category/:id', function ($id) use ($app, $db) {
 
 $app->delete('/category/:id', function ($id) use ($app, $db) {
 	// TODO: Kontrollera om användaren har behörighet att ta bort categori
-	$db->delete('category', array('id' => $id));
+	$db->delete('Customer_categories', array('id' => $id));
 });
 ?>
