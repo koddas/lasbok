49ea718fad1772ce55b13cd00a73117e719345c7
koddas <johan@idioti.se>
2014-04-16 13:42:27 +0200

    Lade till en tom config-fil igen.

---
+++ backend/config/config.php
@@ -0,0 +1,13 @@
+<?php
+$config = array(
+		'slim' => array(),
+		'medoo' => array(
+				'database_type' => 'mysql',
+				'database_name' => '',
+				'server' => 'localhost',
+				'username' => '',
+				'password' => '',
+				'charset' => 'utf8'
+		)
+);
+?>
