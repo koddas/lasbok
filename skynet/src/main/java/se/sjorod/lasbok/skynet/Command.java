24e55505d76f23ad31345d236da11d50ea93010e
koddas <johan@idioti.se>
2014-04-18 22:19:17 +0200

    Skrev tester för Command och implementerade klassen. Lade dessutom till dokumentation för den.

--- skynet/src/main/java/se/sjorod/lasbok/skynet/Command.java
+++ skynet/src/main/java/se/sjorod/lasbok/skynet/Command.java
@@ -1,7 +1,10 @@
 package se.sjorod.lasbok.skynet;
 
-// TODO: Test and document this class
-
+/**
+ * The Command class represents a command sent to the command dispatcher.
+ * 
+ * @author johan
+ */
 public class Command {
 	public static final int LOCK_ALL = 0;
 	public static final int UNLOCK_ALL = 1;
@@ -12,24 +15,58 @@ public class Command {
 	private int command;
 	private Payload payload;
 	
-	public Command(int command) {
+	/**
+	 * Creates an instance of Command.
+	 * 
+	 * @param command The command to send.
+	 * 
+	 * @throws IllegalArgumentException if the arguments were illegal.
+	 */
+	public Command(int command) throws IllegalArgumentException {
+		if (command < 0 || command > UPDATE_SCHEDULE) {
+			throw new IllegalArgumentException();
+		}
+		
 		this.command = command;
 		payload = null;
 	}
 	
-	public Command(int command, Payload payload) {
+	/**
+	 * Creates an instance of Command.
+	 * 
+	 * @param command The command to send.
+	 * @param payload The payload to send with the command.
+	 * 
+	 * @throws IllegalArgumentException if the arguments were illegal.
+	 */
+	public Command(int command, Payload payload) throws IllegalArgumentException {
+		if (command < 0 || command > UPDATE_SCHEDULE) {
+			throw new IllegalArgumentException();
+		}
+		
 		this.command = command;
 		this.payload = payload;
 	}
 	
+	/**
+	 * Gets the command sent with the object.
+	 * 
+	 * @return An integer representing the command.
+	 */
 	public int getCommand() {
 		return command;
 	}
 	
+	/**
+	 * Gets the payload sent with the command.
+	 * 
+	 * @return The payload object.
+	 */
 	public Payload getPayload() {
 		return payload;
 	}
 	
+	@Override
 	public String toString() {
 		return payload == null ? "Command: " + command :
 				"Command: " + command + ", payload: " + payload.toString();
--- skynet/src/test/java/se/sjorod/lasbok/skynet/tests/CommandTest.java
+++ skynet/src/test/java/se/sjorod/lasbok/skynet/tests/CommandTest.java
@@ -2,33 +2,150 @@ package se.sjorod.lasbok.skynet.tests;
 
 import static org.junit.Assert.*;
 
+import mockit.*;
+
 import org.junit.Test;
 
-public class CommandTest {
+import se.sjorod.lasbok.skynet.Command;
+import se.sjorod.lasbok.skynet.Payload;
 
+public class CommandTest {
 	@Test
 	public void testCommandInt() {
-		fail("Not yet implemented");
+		Command command = null;
+		
+		try {
+			command = new Command(Command.UNLOCK);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+		
+		assertEquals(Command.UNLOCK, command.getCommand());
+	}
+
+	@Test
+	public void testCommandIntNegativeValue() {
+		try {
+			Command command = new Command(-1);
+			fail("This shouldn't happen");
+		} catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void testCommandIntZero() {
+		Command command = null;
+		
+		try {
+			command = new Command(0);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+		
+		assertEquals(0, command.getCommand());
+	}
+
+	@Test
+	public void testCommandInHighestValue() {
+		Command command = null;
+		
+		try {
+			command = new Command(Command.UPDATE_SCHEDULE);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+		
+		assertEquals(Command.UPDATE_SCHEDULE, command.getCommand());
+	}
+
+	@Test
+	public void testCommandIntValueTooHigh() {
+		try {
+			Command command = new Command(Command.UPDATE_SCHEDULE + 1);
+			fail("This shouldn't happen");
+		} catch (IllegalArgumentException e) {}
 	}
 
 	@Test
 	public void testCommandIntPayload() {
-		fail("Not yet implemented");
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		try {
+			Command command = new Command(Command.UNLOCK, payload);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+	}
+
+	@Test
+	public void testCommandIntNegativeValuePayload() {
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		try {
+			Command command = new Command(-1, payload);
+			fail("This shouldn't happen");
+		} catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void testCommandIntZeroPayload() {
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		try {
+			Command command = new Command(0, payload);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+	}
+
+	@Test
+	public void testCommandInHighestValuePayload() {
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		try {
+			Command command = new Command(Command.UPDATE_SCHEDULE, payload);
+		} catch (IllegalArgumentException e) {
+			fail("This shouldn't happen");
+		}
+	}
+
+	@Test
+	public void testCommandIntValueTooHighPayload() {
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		try {
+			Command command = new Command(Command.UPDATE_SCHEDULE + 1, payload);
+			fail("This shouldn't happen");
+		} catch (IllegalArgumentException e) {}
 	}
 
 	@Test
 	public void testGetCommand() {
-		fail("Not yet implemented");
+		Command command = new Command(Command.UNLOCK);
+		
+		assertEquals(Command.UNLOCK, command.getCommand());
 	}
 
 	@Test
 	public void testGetPayload() {
-		fail("Not yet implemented");
+		Payload payload = new MockUp<Payload>() {}.getMockInstance();
+		Command command = new Command(Command.UNLOCK, payload);
+		
+		assertNotNull(command.getPayload());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		Command command = new Command(Command.UNLOCK);
+		
+		assertEquals("Command: 3", command.toString());
 	}
+
+//	@Injectable Payload payload;
+//	@Test
+//	public void testToStringWithPayload() {
+//		Command command = new Command(Command.UNLOCK);
+//		
+//		new Expectations() {{
+//			payload.toString();
+//			result = "mock";
+//		}};
+//		
+//		assertEquals("Command: 3, payload: mock", command.toString());
+//	}
 
 }
