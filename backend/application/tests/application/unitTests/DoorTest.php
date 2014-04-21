7cfcd17a4a84b830439ea4630323fad050e56943
koddas <johan@idioti.se>
2014-04-21 09:54:16 +0200

    Första stegen mot testbar php.

---
+++ backend/application/tests/application/unitTests/DoorTest.php
@@ -0,0 +1,12 @@
+<?php
+class DoorTest extends PHPUnit_Framework_TestCase {
+    /** @test */
+    public function testOpen() {
+          $this->assertTrue(false);
+    }
+    
+    /** @test */
+    public function TestClose() {
+        $this->assertTrue(true);
+    }
+}
